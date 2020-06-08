e4241fdc7d1bdd858e27e964dc93d9f0be0d7cd1
shna rafeeq <shnar.majeed@gmail.com>
2020-06-08 06:28:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+ const thingsTodo = `This Saturday, I want to ${activity}!`;
+ return thingsTodo;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(work = 'go to the office'){
+ const workTodo = `This Monday, I will ${work}.`;
+ return workTodo;
+}
+mondayWork('work from home');
+
+function wrapAdjective(vflair = "*"){
+  return function(status = 'special'){
+    let person = `You are ${vflair}${status}${vflair}!`;
+      return person;
+  }
+}
+wrapAdjective("%")("a hard worker")
+
+
+let arrayOfTransforms = [
+          function(a){ return a * 2 },
+          function(a){ return a + 1000},
+          function(a){ return a % 7 }
+        ];
+  
+function actionApplyer(start = 0,arr = []){
+  console.log(arr)
+  for(let i = 0; i<arr.length;i++){
+    start = arr[i](start);
+  }
+  return start;
+}
+actionApplyer(13, arrayOfTransforms);
+
+  let Calculator = {
+    add: (x,y) => {
+      return x + y
+    },
+    subtract: (x,y) => {
+      return x - y
+    },
+    multiply: (x,y) => {
+      return x * y
+    },
+    divide: (x,y) => {
+      return x / y
+    }
+  }
+  let result = [Calculator.add(1,3), Calculator.subtract(1,3), Calculator.multiply(1,3), Calculator.divide(10,5)];
+ console.log(result);
